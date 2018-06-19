e12a74d555815d4f71af126618a212e0cd7cc0b4
Alexey Demchenko <38159234+BarneyGz@users.noreply.github.com>
2018-06-20 00:49:23 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
 	$(document).on('click', '.reposlist__block__readmore', function(event) {
 			$(this.nextElementSibling).find('li').toggle();
+			var targetbtn = $(event.target);
+				if(targetbtn[0].innerHTML == 'Read more...') {
+					targetbtn.html('Hide');
+				}
+				else {
+					targetbtn.html('Read more...');
+				}
 		});
 	
 	$(document).on('keyup', '#searchUser', function(e){ 
@@ -21,33 +28,7 @@ $(document).ready(function(){
         
         	}
       	}).done(function(repos){
-			$.each(repos, function(index, repo){
-				$(".reposlist").append(`
-				<div class="reposlist__block">
-					<a href="${repo.html_url}" target="_blank" class="repos__link">${repo.name}</a> 
-					<hr>
-					
-					${repo.description} 
-					<br>
-
-					<ul>
-						<li>
-							<button class="reposlist__block__readmore" data-toggle="modal" data-target="#Test">Read more...</button>
-							<ul class="${repo.id}">
-								<li class="reposlist__li">Name:  ${repo.name} <br></li>
-								<li class="reposlist__li">Full Name: ${repo.full_name} <br></li>
-								<li class="reposlist__li">Clone: ${repo.clone_url} <br></li>
-								<li class="reposlist__li">Date of creation: ${repo.created_at} <br></li>
-								<li class="reposlist__li">Language: <span style='color:#ffd800;background-color: #d9ecda;'>${repo.language}</span> <br></li>
-								<li class="reposlist__li">Open issues: ${repo.open_issues_count} <br></li>
-								<li class="reposlist__li">Forks count: ${repo.forks_count} <br></li>
-							</ul>
-						</li>
-					</ul>
-				</div> `);
-			});
-      });
-			$('.result').html(`
+				$('.result').html(`
 				<span class="result__userlogin">${user.login}</span><br>
 				<img src="${user.avatar_url}" class="user__avatar"></img>
 				Name: ${user.name} <br>
@@ -59,6 +40,34 @@ $(document).ready(function(){
 				<div class="space"></div>
 				<div class="reposlist"> </div>
 				`);
+			$.each(repos, function(index, repo){
+				$(".reposlist").prepend(`
+				<div class="reposlist__block">
+					<a href="${repo.html_url}" target="_blank" class="repos__link">${repo.name}</a> 
+					<hr>
+					<span class="reposlist__block__description"> Description:  </span>
+					${repo.description} 
+					<br>
+					<ul>
+						<li>
+							<button class="reposlist__block__readmore" data-toggle="modal" ">Read more...</button>
+							<ul class="reposlist__ul">
+								<li class="reposlist__li"><b class="reposlist__li__details">Name: </b> ${repo.name} <br></li>
+								<li class="reposlist__li"><b class="reposlist__li__details">Full Name:</b> ${repo.full_name} <br></li>
+								<li class="reposlist__li"><b class="reposlist__li__details">Clone:</b> <a href="${repo.clone_url}" class="reposlist__li__cloneURL">${repo.clone_url}</a>  <br></li>
+								<li class="reposlist__li"><b class="reposlist__li__details">Date of creation:</b> ${repo.created_at} <br></li>
+								<li class="reposlist__li"><b class="reposlist__li__details">Language:</b> <span class="reposlist__li__language">${repo.language}</span> <br></li>
+								<li class="reposlist__li"><b class="reposlist__li__details">Open issues:</b> ${repo.open_issues_count} <br></li>
+								<li class="reposlist__li"><b class="reposlist__li__details">Forks count:</b> ${repo.forks_count} <br></li>
+							</ul>
+						</li>
+					</ul>
+				</div> `);
+				
+				
+			});
+      });
+		
 		});
 	});
 });
